07c5b7c7ee47927a0d31c6eac7a65f6a9bb83b3b
João Bispo <jsbispo1101@gmail.com>
2024-03-28 14:03:46 -0400

    initial commit

---
+++ API.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import cors from "cors";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+env.config();
+
+const db = new pg.Client({
+    connectionString: process.env.PG_URL,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", async (req, res) => {
+    const result = await db.query("SELECT * FROM todolist");
+    const data = result.rows;
+
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
+});
